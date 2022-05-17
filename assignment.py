edf8d34c4e5f0698ba41c4ff8f9ae6da12f5758f
Yuhui Pang <pyh980425@163.com>
2022-05-17 21:38:43 +1000

    q4 predict

--- assignment.py
+++ assignment.py
@@ -9,8 +9,6 @@ u7167784
 """
 import csv
 
-from numpy import sort
-
 
 def analyse(path_to_file):
     print("Analysing data file", path_to_file)
@@ -19,12 +17,12 @@ def analyse(path_to_file):
         table = [row for row in reader]
     table_without_header = [table[i] for i in range(1, len(table))]
     # Question 1
-    # print("\nQuestion 1:")
-    # unique_locations = count_unique_locations(table_without_header)
-    # unique_countries = count_unique_countries(table_without_header)
-    # global_vaccince_doses(table_without_header)
-    # global_population_vaccinated(table_without_header)
-    # global_fully_population_vaccinated(table_without_header)
+    print("\nQuestion 1:")
+    unique_locations = count_unique_locations(table_without_header)
+    unique_countries = count_unique_countries(table_without_header)
+    global_vaccince_doses(table_without_header)
+    global_population_vaccinated(table_without_header)
+    global_fully_population_vaccinated(table_without_header)
     # Question 2
     print("\nQuestion 2:")
     vaccinations_by_country(table_without_header)
@@ -45,10 +43,11 @@ def count_unique_locations(table):
     Parameter locations: A list of locations (Duplicate removal).
     Return a total number of unique locations.
     """
-    locations = [row[0] for row in table] # A list of locations 
-    locations = list(set(locations)) # Duplicate removal
+    locations = [row[0] for row in table]  # A list of locations
+    locations = list(set(locations))  # Duplicate removal
     print("Number of unique locations:", len(locations))
     return len(locations)
+
 
 # 1b
 def count_unique_countries(table):
@@ -58,14 +57,15 @@ def count_unique_countries(table):
     Parameter countries: A list of countries (Duplicate removal).
     Return a total number of unique countries.
     """
-    countries = [row[1] for row in table] # A list of countries
-    countries = list(set(countries)) # Duplicate removal
+    countries = [row[1] for row in table]  # A list of countries
+    countries = list(set(countries))  # Duplicate removal
     count = 0
     for country in countries:
-        if "OWID_" not in country: # Check countries do not have OWID_ prefix
+        if "OWID_" not in country:  # Check countries do not have OWID_ prefix
             count += 1
     print("Number of unique countries:", count)
     return count
+
 
 # 1c
 def global_vaccince_doses(table):
@@ -80,11 +80,12 @@ def global_vaccince_doses(table):
     countries_total_vax = [[row[1], row[3]] for row in table if "OWID_" not in row[1] and row[3] != ""]
     result = []
     for i in range(len(countries_total_vax)):
-        if(i == len(countries_total_vax)-1 or countries_total_vax[i][0] != countries_total_vax[i+1][0]):
+        if (i == len(countries_total_vax) - 1 or countries_total_vax[i][0] != countries_total_vax[i + 1][0]):
             result.append(countries_total_vax[i])
     count = [int(row[1]) for row in result]
     print("Global vaccince doses:", sum(count))
     return sum(count)
+
 
 # 1d
 def global_population_vaccinated(table):
@@ -99,7 +100,8 @@ def global_population_vaccinated(table):
     countries_people_vaccinated = [[row[1], row[4]] for row in table if "OWID_" not in row[1] and row[4] != ""]
     result = []
     for i in range(len(countries_people_vaccinated)):
-        if(i == len(countries_people_vaccinated)-1 or countries_people_vaccinated[i][0] != countries_people_vaccinated[i+1][0]):
+        if (i == len(countries_people_vaccinated) - 1 or countries_people_vaccinated[i][0] !=
+                countries_people_vaccinated[i + 1][0]):
             result.append(countries_people_vaccinated[i])
     count = [int(row[1]) for row in result]
     print("Global population vaccinated:", sum(count))
@@ -118,7 +120,8 @@ def global_fully_population_vaccinated(table):
     countries_people_fully_vaccinated = [[row[1], row[5]] for row in table if "OWID_" not in row[1] and row[5] != ""]
     result = []
     for i in range(len(countries_people_fully_vaccinated)):
-        if(i == len(countries_people_fully_vaccinated)-1 or countries_people_fully_vaccinated[i][0] != countries_people_fully_vaccinated[i+1][0]):
+        if (i == len(countries_people_fully_vaccinated) - 1 or countries_people_fully_vaccinated[i][0] !=
+                countries_people_fully_vaccinated[i + 1][0]):
             result.append(countries_people_fully_vaccinated[i])
     count = [int(row[1]) for row in result]
     print("Global population fully vaccinated:", sum(count))
@@ -127,22 +130,25 @@ def global_fully_population_vaccinated(table):
 
 # 2ab
 def vaccinations_by_country(table):
-    countries_vaccinations_rate = [[row[0], row[4], float(row[10]), round(float(row[10])-float(row[11]), 2)] for row in table if "OWID_" not in row[1] and row[4] != "" 
-    and row[10] != "" and row[11] != "" and float(row[10]) != 0.0 and int(row[4])/float(row[10]) > 10000]
+    countries_vaccinations_rate = [[row[0], row[4], float(row[10]), round(float(row[10]) - float(row[11]), 2)] for row
+                                   in table if "OWID_" not in row[1] and row[4] != ""
+                                   and row[10] != "" and row[11] != "" and float(row[10]) != 0.0 and int(
+            row[4]) / float(row[10]) > 10000]
     result = []
     for i in range(len(countries_vaccinations_rate)):
-        if(i == len(countries_vaccinations_rate)-1 or countries_vaccinations_rate[i][0] != countries_vaccinations_rate[i+1][0]):
+        if (i == len(countries_vaccinations_rate) - 1 or countries_vaccinations_rate[i][0] !=
+                countries_vaccinations_rate[i + 1][0]):
             result.append(countries_vaccinations_rate[i])
-    result.sort(key = lambda x:x[2],reverse=True)
+    result.sort(key=lambda x: x[2], reverse=True)
     # 2a
     for row in result[0:10]:
-        print(row[0]+": "+str(row[2])+"% population vaccinated")
+        print(row[0] + ": " + str(row[2]) + "% population vaccinated")
     # 2b
     for row in result[0:10]:
-        print(row[0]+": "+str(row[2])+"% population vaccinated,"+str(row[3])+"% partly vaccinated")
+        print(row[0] + ": " + str(row[2]) + "% population vaccinated," + str(row[3]) + "% partly vaccinated")
 
     return result[0:10]
-    
+
 
 # 2b
 # def population_partly_vaccinated_by_country(table):
@@ -152,14 +158,14 @@ def vaccinations_by_country(table):
 #     for i in range(len(countries_1stvaccinations_rate)):
 #         if i == len(countries_1stvaccinations_rate) -1 or countries_1stvaccinations_rate[i][0] != countries_1stvaccinations_rate[i+1][0]:
 #             result.append(countries_1stvaccinations_rate[i])
-    # print(result)
+# print(result)
 
-    # top10_countries = vaccinations_by_country(table)
+# top10_countries = vaccinations_by_country(table)
 
-    # for i in range(len(top10_countries)):
-    #     for j in range(len(result)):
-    #         if result[j][0] == top10_countries[i][0]:
-    #             print(top10_countries[i][0]+": "+str(top10_countries[i][2])+"% population vaccinated,"+str(result[j][1])+"% partly vaccinated")
+# for i in range(len(top10_countries)):
+#     for j in range(len(result)):
+#         if result[j][0] == top10_countries[i][0]:
+#             print(top10_countries[i][0]+": "+str(top10_countries[i][2])+"% population vaccinated,"+str(result[j][1])+"% partly vaccinated")
 
 
 # def fun(x):
@@ -167,28 +173,30 @@ def vaccinations_by_country(table):
 
 # 3ab
 def earliest_countries(table):
-    countries_date = [[row[0], row[8],row[2]] for row in table 
-    if "OWID_" not in row[1] and row[3] != "" and int(row[3]) > 0]
+    countries_date = [[row[0], row[8], row[2]] for row in table
+                      if "OWID_" not in row[1] and row[3] != "" and int(row[3]) > 0]
     result = []
     # result.append(countries_date[0])
     for i in range(len(countries_date)):
-        if(i == 0 or countries_date[i][0] != countries_date[i-1][0]):
+        if (i == 0 or countries_date[i][0] != countries_date[i - 1][0]):
             result.append(countries_date[i])
     # top10_countries_date = topN(result, 1, 10, False)
     # for countries_date in top10_countries_date:
     #     print(countries_date[0]+": first vaccinated on",countries_date[1])
 
-    result.sort(key = lambda x:x[2])
+    result.sort(key=lambda x: x[2])
     # result.sort(key = fun)
 
     # 3a
     for row in result[0:10]:
-        print(row[0]+": first vaccinated on",row[2])
+        print(row[0] + ": first vaccinated on", row[2])
     # 3b
     for country_date in result[0:10]:
-        each_country_daily_vaccinations = [[row[0],int(row[1]),row[2]] for row in countries_date if country_date[0] == row[0] and row[1] != ""]
+        each_country_daily_vaccinations = [[row[0], int(row[1]), row[2]] for row in countries_date if
+                                           country_date[0] == row[0] and row[1] != ""]
         max_daily_vaccinations = max(each_country_daily_vaccinations)
-        print(country_date[0] + ": first vaccinated on " + country_date[2] + " , " + str(max_daily_vaccinations[1]) + " people vaccinated on " + max_daily_vaccinations[2])
+        print(country_date[0] + ": first vaccinated on " + country_date[2] + " , " + str(
+            max_daily_vaccinations[1]) + " people vaccinated on " + max_daily_vaccinations[2])
 
     return result[0:10]
     # date_list = [row[1] for row in result]
@@ -202,6 +210,7 @@ def earliest_countries(table):
     #             top10_countries_date.append([row[0], date_value])
     #             print(row[0]+": first vaccinated on",date_value)
     # return top10_countries_date
+
 
 # 3b
 # def peak_days(table):
@@ -233,17 +242,21 @@ def earliest_countries(table):
 
 # 4ab
 def fully_vaccinated_rate(table):
-    countries_fully_vaccinated = [[row[0], float(row[11])] for row in table if "OWID_" not in row[1] and row[11] != "" and float(row[11]) > 50]
+    countries_fully_vaccinated = [[row[0], float(row[11])] for row in table if
+                                  "OWID_" not in row[1] and row[11] != "" and float(row[11]) > 50]
     result = []
     # result.append(countries_date[0])
     for i in range(len(countries_fully_vaccinated)):
-        if(i == len(countries_fully_vaccinated)-1 or countries_fully_vaccinated[i][0] != countries_fully_vaccinated[i+1][0]):
+        if (i == len(countries_fully_vaccinated) - 1 or countries_fully_vaccinated[i][0] !=
+                countries_fully_vaccinated[i + 1][0]):
             result.append(countries_fully_vaccinated[i])
-    result.sort(key = lambda x:x[1])
+    result.sort(key=lambda x: x[1])
     # print(result[0:10])
     # 4a
     for row in result[0:10]:
-        print(row[0]+": "+str(row[1])+"% population fully vaccinated")
+        print(row[0] + ": " + str(row[1]) + "% population fully vaccinated")
+
+
 # The section below will be executed when you run this file.
 # Use it to run tests of your analysis function on the data
 # files provided.
---
+++ linearRegression.py
@@ -0,0 +1,61 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import datetime as dt
+
+
+def predict(path_to_file):
+    print("Analysing data file", path_to_file)
+    data = pd.read_csv(path_to_file)
+    analyse(data, "Japan", 100)
+
+
+def analyse(data, country, rate):
+    var = data.loc[data['location'] == country, ['date', 'people_fully_vaccinated_per_hundred']]
+    var['date'] = pd.to_datetime(var['date'])
+    var['date'] = var['date'].map(dt.datetime.toordinal)
+    pds = pd.DataFrame(data=var)
+    a = pds.dropna()
+
+    x = a['date'].values.reshape(-1, 1)
+    y_ = a['people_fully_vaccinated_per_hundred'].values.reshape(-1, 1)
+
+    reg = LinearRegression()
+    reg.fit(x, y_)
+    print('a = {:.5}'.format(reg.coef_[0][0]))  # coef_回归系数
+    print('b = {:.5}'.format(reg.intercept_[0]))  # intercept_截距
+    print("线性模型为: y_ = {:.5}x + {:.5} ".format(reg.coef_[0][0], reg.intercept_[0]))
+    predictions = reg.predict(x)
+
+    plt.scatter(a['date'], a['people_fully_vaccinated_per_hundred'], c='black')
+    plt.plot(a['date'], predictions, c='blue', linewidth=2)  # 蓝线
+    plt.xlabel("date")
+    plt.ylabel("vaccinated")
+    plt.show()
+
+    # 预测
+    # predictions = reg.predict([[8000]])
+    # print(predictions)
+    # print('8000米高度的温度{:.5}'.format(predictions[0][0]))
+
+    # 初始化预测值为last day，并输出last day的接种比例
+    result = x[-1]
+    print(reg.predict([result]))
+
+    # 循环直到接种比例大于阈值，输出日期和天数
+    while reg.predict([result]) < rate:
+        result = result + 1
+    print(dt.datetime.fromordinal(result[0]))
+    print(result - x[-1])
+
+
+def date(para):
+    delta = pd.Timedelta(str(int(para)) + 'days')
+    time = pd.to_datetime('1899-12-30') + delta
+    return time
+
+
+if __name__ == '__main__':
+    # test on a CSV file
+    predict('./vaccinations2.csv')
